102001efbd57441589554ef9a45d8f126ca6f967
Nishan <nishanbende@gmail.com>
2016-12-23 14:36:15 +0530

    StateController Updated

--- src/com/spring/controller/StateController.java
+++ src/com/spring/controller/StateController.java
@@ -1,13 +1,27 @@
 package com.spring.controller;
 
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.DAO.CountryDAO;
+import com.spring.DAO.StateDAO;
+import com.spring.VO.StateVO;
 
 @Controller
 public class StateController {
 
-
+	@Autowired
+	StateDAO state;
+	CountryDAO country;
+	
 	@RequestMapping(value="/viewState.html" , method=RequestMethod.GET)
 	public String loadState()
 	{
@@ -16,9 +30,36 @@ public class StateController {
 	}
 
 	@RequestMapping(value="/addState.html" , method=RequestMethod.GET)
-	public String addState()
+	public ModelAndView addState(HttpSession session)
+	{	
+		try {
+			List<Object> ls = this.country.getCountry();
+			session.setAttribute("ls", ls);
+			
+			
+		
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return new ModelAndView("admin/addState","insertState",new StateVO());
+		
+	}
+	@RequestMapping(value="/insertState.html" , method=RequestMethod.POST)
+	public String insertState(@ModelAttribute StateVO insertState)
 	{
-		return("admin/addState");
+		
+		try {
+			state.insertState(insertState);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return ("redirect:/addState.html");	
+		
+		
 		
 	}
 }
