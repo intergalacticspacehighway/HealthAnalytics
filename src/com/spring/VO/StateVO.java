fe14d1f9d3661edcfcdd871c8f1c22a1e13380a5
parth02 <parththakkar02@gmail.com>
2016-12-23 12:28:53 +0530

    statevo changed

--- src/com/spring/VO/StateVO.java
+++ src/com/spring/VO/StateVO.java
@@ -1,5 +1,48 @@
 package com.spring.VO;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "state")
 public class StateVO {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int stateId;
+	@Column
+	private String stateName;
+
+	@ManyToOne
+	CountryVO country;
+
+	public CountryVO getCountry() {
+		return country;
+	}
+
+	public void setCountry(CountryVO country) {
+		this.country = country;
+	}
+
+	public int getStateId() {
+		return stateId;
+	}
+
+	public void setStateId(int stateId) {
+		this.stateId = stateId;
+	}
+
+	public String getStateName() {
+		return stateName;
+	}
+
+	public void setStateName(String stateName) {
+		this.stateName = stateName;
+	}
 
 }
