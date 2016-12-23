82ef822cdf7f4f2537b4f98b8a12450bf4f58d72
Nishan <nishanbende@gmail.com>
2016-12-23 13:36:35 +0530

    CountryController Updated

--- src/com/spring/controller/CountryContoller.java
+++ src/com/spring/controller/CountryContoller.java
@@ -1,11 +1,20 @@
 package com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.DAO.CountryDAO;
+import com.spring.VO.CountryVO;
 
 @Controller
 public class CountryContoller {
+	
+	@Autowired
+	CountryDAO country;
 
 	@RequestMapping(value="/viewCountry.html" , method=RequestMethod.GET)
 	public String loadCountry()
@@ -18,6 +27,16 @@ public class CountryContoller {
 	public String addCountry()
 	{
 		return("admin/addCountry");
+		
+	}
+	
+	@RequestMapping(value="/insertCountry.html" , method=RequestMethod.GET)
+	public ModelAndView insertCountry(@ModelAttribute CountryVO insertCountry)
+	{
+		ModelAndView model = new ModelAndView("admin/addCountry");
+		
+		return model;
+		
 		
 	}
 }
