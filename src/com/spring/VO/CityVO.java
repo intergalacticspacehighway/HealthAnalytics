4400b26e3cad5f576b8499870413537ec84e2fa2
DpakJaswani <djaswani96@gmail.com>
2016-12-23 12:37:24 +0530

    ...

--- src/com/spring/VO/CityVO.java
+++ src/com/spring/VO/CityVO.java
@@ -5,6 +5,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -15,7 +16,8 @@ public class CityVO {
 	private int cityId;
 	@Column
 	private String cityName;
-	private StateVO state = new StateVO();
+	@ManyToOne
+	StateVO state;
 
 	public int getCityId() {
 		return cityId;
@@ -40,6 +42,5 @@ public class CityVO {
 	public void setState(StateVO state) {
 		this.state = state;
 	}
-
 
 }
