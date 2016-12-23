d6ff6c6ebd6ca0bc657fd3a54dcfedff976630be
Nishan <nishanbende@gmail.com>
2016-12-23 12:29:24 +0530

    CountryVO commited

--- src/com/spring/VO/CountryVO.java
+++ src/com/spring/VO/CountryVO.java
@@ -1,5 +1,35 @@
 package com.spring.VO;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "country")
 public class CountryVO {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int Countryid;
+	@Column
+	private String countryName;
+
+	public int getCountryid() {
+		return Countryid;
+	}
+
+	public void setCountryid(int countryid) {
+		Countryid = countryid;
+	}
+
+	public String getCountryName() {
+		return countryName;
+	}
+
+	public void setCountryName(String countryName) {
+		this.countryName = countryName;
+	}
 
 }
