51c0181fbe5ebc029bd1295d2d4c0bd2d174e296
parth02 <parththakkar02@gmail.com>
2016-12-29 22:16:58 +0530

    disease inserted

--- src/com/spring/controller/DiseaseController.java
+++ src/com/spring/controller/DiseaseController.java
@@ -1,11 +1,20 @@
 package com.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.spring.DAO.DiseaseDAO;
+import com.spring.VO.DiseaseVO;
 
 @Controller
 public class DiseaseController {
+	
+	@Autowired
+	DiseaseDAO disease;
 
 	@RequestMapping(value="/viewDisease.html" , method=RequestMethod.GET)
 	public String loadDisease()
@@ -15,9 +24,16 @@ public class DiseaseController {
 	}
 
 	@RequestMapping(value="/addDisease.html" , method=RequestMethod.GET)
-	public String addDisease()
+	public ModelAndView addDisease()
 	{
-		return("admin/addDisease");
+		return new ModelAndView("admin/addDisease","insertDisease",new DiseaseVO());
+		
+	}
+	@RequestMapping(value="/insertDisease.html" , method=RequestMethod.POST)
+	public String insertDisease(@ModelAttribute DiseaseVO insertDisease)
+	{
+		this.disease.insertDisease(insertDisease);
+		return("redirect:/addDisease.html");
 		
 	}
 }
