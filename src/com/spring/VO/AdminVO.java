204ee86e08a6fb7bff118aedb472c0e0ecf0d485
Nishan <nishanbende@gmail.com>
2017-01-07 12:12:10 +0530

    AdminVO and client folder

---
+++ src/com/spring/VO/AdminVO.java
@@ -0,0 +1,36 @@
+package com.spring.VO;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="admin")
+
+public class AdminVO {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int adminId;
+	public int getAdminId() {
+		return adminId;
+	}
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+	public String getAdminName() {
+		return adminName;
+	}
+	public void setAdminName(String adminName) {
+		this.adminName = adminName;
+	}
+	public String getAdminPassword() {
+		return adminPassword;
+	}
+	public void setAdminPassword(String adminPassword) {
+		this.adminPassword = adminPassword;
+	}
+	private String adminName;
+	private String adminPassword;
+}
